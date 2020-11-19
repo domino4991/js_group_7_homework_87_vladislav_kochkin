30756780c56704a4fa3e593223dd29f1fac03574
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-11-19 19:26:44 +0600

    Редюсеры и экшены для юзеров и комментов

---
+++ frontend/src/store/reducers/commentsReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    comments: null,
+    error: null
+};
+
+export const commentsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
---
+++ frontend/src/store/reducers/usersReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    user: null,
+    error: null
+};
+
+export const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
