4beae2ee5a6aa921019cb1ce7b150397688d33b5
Kochkin Vladislav <ltrickfaim@gmail.com>
2020-11-20 17:30:04 +0600

    Добавил количество комментариев к посту

--- backend/app/posts.js
+++ backend/app/posts.js
@@ -5,6 +5,7 @@ const multer = require('multer');
 const {nanoid} = require('nanoid');
 const auth = require('../auth');
 const Post = require('../models/Post');
+const Comment = require('../models/Comment');
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -22,9 +23,14 @@ router.get('/', async (req, res) => {
         const posts = await Post
             .find()
             .sort({datetime: -1})
-            .populate('author', 'name')
-            .limit(20);
+            .populate('author', 'name -_id')
+            .limit(20)
+            .lean();
         if(!posts && posts.length === 0) return res.send({message: 'Еще нет ни одного поста'});
+        for(let postItem of posts) {
+            const comments = await Comment.find({post: postItem._id});
+            postItem.countComment = comments.length;
+        }
         return res.send(posts);
     } catch (e) {
         return res.status(500).send(e);
@@ -35,11 +41,11 @@ router.get('/:id', async (req, res) => {
     try {
         const post = await Post
             .findOne({_id: req.params.id})
-            .populate('author', 'name');
+            .populate('author', 'name -_id');
         if(!post) return res.status(404).send({error: 'Пост не найден'});
         return res.send(post);
     } catch (e) {
-        return res.status(500).send(e);
+        return res.status(400).send({error: 'Не верно указан id поста'});
     }
 });
 
